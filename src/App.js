45d6a243a1d5f09fc4e61f9fa61907f6ec57526c
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-03-06 17:54:48 -0300

    Requisito 5 feito

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ class App extends React.Component {
     cardAttr2: '',
     cardAttr3: '',
     cardImage: '',
-    cardRare: '',
-    cardTrunfo: '',
+    cardRare: 'normal',
+    cardTrunfo: false,
+    isSaveButtonDisabled: true,
   };
 
   onInputChange = ({ target }) => {
@@ -19,7 +20,60 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.validateFields);
+  };
+
+  // validateFields = () => {
+  //   const { title, description } = this.state;
+  //   const MIN_TITLE_LENGTH = 4;
+  //   const MIN_DESCRIPTION_LENGTH = 10;
+  //   if (title.length > MIN_TITLE_LENGTH && description.length > MIN_DESCRIPTION_LENGTH) {
+  //     this.setState ({
+  //       disableButton: false,
+  //     })
+  //   } else {
+  //     this.setState ({
+  //       disableButton: true,
+  //     })
+  //   }
+  // }
+
+  validateFields = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+    const MIN_ATTR = 0;
+    const MAX_ATTR = 90;
+    const TOTAL_ATTR = 210;
+    const sumAttr = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    const isLessThenMaxValue = sumAttr <= TOTAL_ATTR;
+    const verifyAttr1 = cardAttr1 >= MIN_ATTR && cardAttr1 <= MAX_ATTR;
+    const verifyAttr2 = cardAttr2 >= MIN_ATTR && cardAttr2 <= MAX_ATTR;
+    const verifyAttr3 = cardAttr3 >= MIN_ATTR && cardAttr3 <= MAX_ATTR;
+
+    if (cardName !== ''
+    && cardDescription !== ''
+    && cardImage !== ''
+    && cardRare !== ''
+    && isLessThenMaxValue
+    && verifyAttr1
+    && verifyAttr2
+    && verifyAttr3
+    ) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
   };
 
   render() {
@@ -32,6 +86,7 @@ class App extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
@@ -46,6 +101,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           cardName={ cardName }
